1f7ada8e1f39bf06af1e75ccc237f1ecb62d22f5
shkarsmode <zshkarrr@gmail.com>
2021-11-10 00:18:04 +0200

    Create group-hero and addHeroGroup

---
+++ src/app/group-hero/group-hero.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+import { Group } from '../interfaces/group';
+import { Hero } from '../interfaces/hero';
+import { HeroService } from '../service/hero.service';
+
+@Component({
+  selector: 'app-group-hero',
+  templateUrl: './group-hero.component.html',
+  styleUrls: ['./group-hero.component.less']
+})
+export class GroupHeroComponent implements OnInit {
+
+  heroes?: Hero[];
+  groups?: Group[];
+  selectHero: any;
+  selectGroup: any;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .subscribe((el:any) => {this.heroes = el; });
+    this.heroService.getGroups()
+      .subscribe((el:any) => {
+        this.groups = el;
+        // this.selectGroup = el[0].name
+      });
+  }
+
+  addHeroGroup(){
+    // let length = Object.entries(this.groups![this.selectGroup-1].id_heroes).length;
+    let arrayIdHeroes: Array<number> = [];
+    Object.entries(this.groups![this.selectGroup-1].id_heroes).forEach(el => {
+      arrayIdHeroes.push(el[1]);
+    })
+    if(!arrayIdHeroes.indexOf(Number(this.selectHero))) console.log('hero is already here');
+    else {
+      arrayIdHeroes.push(Number(this.selectHero));
+      this.heroService.changeHeroList(this.selectGroup, arrayIdHeroes);
+    }
+
+  }
+
+}
